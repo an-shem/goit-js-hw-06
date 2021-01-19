2160801e24e3bff6827c14329f78408f3b67f004
Andrey Shemet <an-shem@ukr.net>
2021-01-20 00:13:17 +0200

    Сделал шесть заданий

--- js/task-dz6.js
+++ js/task-dz6.js
@@ -1,4 +1,6 @@
 import users from './users.js';
+
+console.log(`ЗАДАНИЕ - 1`);
 // ----------1
 const getUserNames = users =>
   users.reduce((acc, user) => {
@@ -9,6 +11,7 @@ const getUserNames = users =>
 console.log(getUserNames(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
 
+console.log(`ЗАДАНИЕ - 2`);
 // ----------2
 const getUsersWithEyeColor = (users, color) =>
   users.filter(user => user.eyeColor === color);
@@ -16,6 +19,7 @@ const getUsersWithEyeColor = (users, color) =>
 console.log(getUsersWithEyeColor(users, 'blue'));
 // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
 
+console.log(`ЗАДАНИЕ - 3`);
 // -----------3
 // const getUsersWithGender = (users, gender) =>
 //   users
@@ -36,8 +40,32 @@ const getUsersWithGender = (users, gender) =>
 console.log(getUsersWithGender(users, 'male'));
 // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
 
+console.log(`ЗАДАНИЕ - 4`);
 // ----------4
 const getInactiveUsers = users => users.filter(user => !user.isActive);
 
 console.log(getInactiveUsers(users));
 // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
+
+console.log(`ЗАДАНИЕ - 5`);
+// ----------5
+const getUserWithEmail = (users, email) =>
+  users.find(user => user.email === email);
+
+console.log(getUserWithEmail(users, 'shereeanthony@kog.com'));
+// {объект пользователя Sheree Anthony}
+console.log(getUserWithEmail(users, 'elmahead@omatom.com'));
+// {объект пользователя Elma Head}
+
+console.log(`ЗАДАНИЕ - 6`);
+// -----------6
+const getUsersWithAge = (users, min, max) =>
+  users.filter(user => user.age >= min && user.age <= max);
+
+console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
+
+console.log(getUsersWithAge(users, 30, 40));
+// [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
+
+console.log(`ЗАДАНИЕ - 7`);
+// -----------7
