3d1851c445a03557f09b6d6ed6d2f0e8e812c180
Andrey Shemet <an-shem@ukr.net>
2021-01-20 01:09:38 +0200

    Исправил задание 3

--- js/task-dz6.js
+++ js/task-dz6.js
@@ -17,21 +17,8 @@ console.log(getUsersWithEyeColor(users, 'blue'));
 
 console.log(`ЗАДАНИЕ - 3`);
 // -----------3
-// const getUsersWithGender = (users, gender) =>
-//   users
-//     .filter(user => user.gender === gender)
-//     .reduce((acc, item) => {
-//       acc.push(item.name);
-//       return acc;
-//     }, []);
-
 const getUsersWithGender = (users, gender) =>
-  users.reduce((acc, user) => {
-    if (user.gender === gender) {
-      acc.push(user.name);
-    }
-    return acc;
-  }, []);
+  users.filter(user => user.gender === gender).map(user => user.name);
 
 console.log(getUsersWithGender(users, 'male'));
 // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
